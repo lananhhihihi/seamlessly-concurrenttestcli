8b1892c7ab57a5cb0e22b5cfe47b0d2f5c53b766
lananhhihihi <drewybarranam1311a@outlook.com>
2024-05-10 16:00:50 +0700

    aptos commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-function isSymmetric(root) {
-  if (!root) return true;
-  return isMirror(root.left, root.right);
-  function isMirror(left, right) {
-    if (!left && !right) return true;
-    if (!left || !right || left.val !== right.val) return false;
-    return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+function numSquares(n) {
+  const dp = new Array(n + 1).fill(Infinity);
+  dp[0] = 0;
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j * j <= i; j++) {
+      dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+    }
   }
+  return dp[n];
 }
