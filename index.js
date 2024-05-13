cbac7a2b28273d9a68936e60664f413f68ec09d1
lananhhihihi <drewybarranam1311a@outlook.com>
2024-05-13 22:49:52 +0700

    First commit

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-function removeDuplicates(nums) {
-  let index = 0;
+function lengthOfLIS(nums) {
+  if (nums.length === 0) return 0;
+  const dp = new Array(nums.length).fill(1);
+  let max = 1;
   for (let i = 1; i < nums.length; i++) {
-    if (nums[i] !== nums[index]) {
-      index++;
-      nums[index] = nums[i];
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+        max = Math.max(max, dp[i]);
+      }
     }
   }
-  return index + 1;
+  return max;
 }
