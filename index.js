6365b687856f21389d7bebb63b7f61eb5713f820
lananhhihihi <drewybarranam1311a@outlook.com>
2024-05-14 14:28:08 +0700

    aptos task

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-function lengthOfLIS(nums) {
-  if (nums.length === 0) return 0;
-  const dp = new Array(nums.length).fill(1);
-  let max = 1;
-  for (let i = 1; i < nums.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (nums[i] > nums[j]) {
-        dp[i] = Math.max(dp[i], dp[j] + 1);
-        max = Math.max(max, dp[i]);
-      }
-    }
-  }
-  return max;
+function factorial(n) {
+  if (n === 0 || n === 1) return 1;
+  return n * factorial(n - 1);
 }
